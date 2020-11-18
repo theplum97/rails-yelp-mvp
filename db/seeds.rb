0587ab479c6f494077d9b107221b00f72003cd9e
Marvin Pflaume <pflaume.marvin@gmail.com>
2020-11-18 16:36:50 +0100

    ..

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do
+  restaurant_new = Restaurant.new(name: Faker::Restaurant.name, address: Faker::Address.full_address, category: ['chinese', 'italian', 'japanese', 'french', 'belgian'].sample)
+  restaurant_new.save
+end
